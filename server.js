c9c180290bbb691a7a13200cc2e362aa6806be9a
mehranbinmoin <mehranmoin@gmail.com>
2025-02-08 15:28:50 -0500

    Created landing page and CSS stylesheet for all pages.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv");
 dotenv.config();
+const path = require("path");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -11,6 +12,12 @@ mongoose.connect(process.env.MONGODB_URI)
 mongoose.connection.on('connected', ()=> {
     console.log(`Connected to MongoDB ${mongoose.connection.name}`);
     
+})
+
+app.use(express.static(path.join(__dirname, 'Public')));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
 })
 
 app.listen(PORT, () => {
